412c80775678ba3d8c7730ec2cb834f7503faf83
AlexTH23 <2236000174@utna.edu.mx>
2025-06-20 23:20:36 -0600

    Se realizó la conexion a mongoDB y se implemento el ignore en configuracion.js

--- app/config/configuracion.js
+++
@@ -1,6 +0,0 @@
-// Encripta la informacion y las oculta
-// En esta parte de configuracion es donde se va  a ir toda la informacion sensible como ip's, contraseñas, etc
-
-module.exports={
-    PORT : process.env.PORT || 3000,
-}
--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const CONFIG = require('./app/config/configuracion')
 //App.js
 const app = require('./app/app')
 
+//Conexion.js
+const conexion = require('./app/config/conexion')
+conexion.conect()
+
 app.listen(CONFIG.PORT, ()=>{
     console.log(`Aplicacion corriendo en puerto ${CONFIG.PORT}`);
 })
+
+
+
